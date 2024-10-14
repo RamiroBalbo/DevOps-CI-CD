3d90af38aab551e21f4a87fa77b0207d4573228b
RamiroBalbo <ramirobalbo48@gmail.com>
2024-10-14 14:14:56 -0300

    ok

--- APPx-API/Minikube/secret.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret docker-registry regcred \
-    --docker-server=https://index.docker.io/v1/
-    --docker-username=ramirobalbo \
-    --docker-password=ferrari450 \
-    --docker-email=ramirobalbo48@gmail.com
